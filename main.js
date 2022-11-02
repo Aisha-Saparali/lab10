7bfa8496ea06308923d64510cb6ce3d083345f29
Aisha <aisha.saparali@nu.edu.kz>
2022-11-02 21:39:08 +0100

    Initial commit

--- main.js
+++ main.js
@@ -8,33 +8,47 @@ let lab6 = [5, 5, 10, 5, 5, 10];
 let lab8 = [5, 10, 10, 9, 5];
 let quizl = 32; 
 
-function get_sum(A){
+function main(lab1,lab2,lab3,lab4and5,lab6,lab8) {
+function get_sum(lab){
 let sum=0;
-for (let i=0; i< A.length; i++) {
-sum=sum+A[i];}
+for (let i=0; i<lab.length; i+=1) {
+	sum+=lab[i];
+}
 return sum;
 }
-
-function main(lab1,lab2,lab3,lab4and5,lab6,lab8,quiz1) {
 sum1=get_sum(lab1);
 sum2=get_sum(lab2);
 sum3=get_sum(lab3);
 sum4and5=get_sum(lab4and5);
 sum6=get_sum(lab6); 
 sum8=get_sum(lab8);
-let avg_labs_score = (sum1+sum2+sum3+sum4and5+sum6+lab8)/6;
-let total_score =0.6*avg_labs_score+0.4*quizl;
-results={
-lab1: [5, 5, 5, 5, 10, 10], 
-lab2: [5, 5, 5, 5, 5, 5, 10],
-lab3: [5, 10, 10, 15], 
-lab4and5: [10, 5, 5, 5, 4, 5, 10, 10, 10, 10, 5], 
-lab6: [5, 5, 10, 5, 5, 10], 
-quiz1: 32,
-sums: sum1,sum2, sum3, sum4and5,sum6, sum8,
-total_sums: total_sums,
-avg_total_sums: avg_total_sums }
-return results}
+quiz1:32;
+let sums = [sum1,sum2,sum3,sum4and5,sum6,sum8];
+function ttl(sums){
+	let ttl=0;
+	for (let a=0; a<sums.length;a+=1){
+		ttl+=sums[a];
+	}
+	return ttl;
+}
 
-console.log (main);
+let total=ttl(sums);
+let avg_labs_score = total/7;
+
+const results={
+lab1: lab1, 
+lab2: lab2,
+lab3: lab3, 
+lab4and5: lab4and5, 
+lab6: lab6, 
+lab8: lab8,
+sums: sums,
+avg_labs_score: avg_labs_score, 
+quiz1: 32, 
+ttl_score: 0.6*avg_labs_score+0.4*quizl}
+return results;
+}
+
+results=main(lab1,lab2,lab3,lab4and5,lab6,lab8);
+console.log(results);
 
